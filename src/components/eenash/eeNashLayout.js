6f99bde53c4351668fe22646307ad0295bae724a
Spencer Rowland <jsrowland10@gmail.com>
2019-07-28 18:45:22 -0500

    fix huge fontawesome icon

--- src/components/eenash/eeNashLayout.js
+++ src/components/eenash/eeNashLayout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { library } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookF, faTwitter, faInstagram, faTripadvisor, faYelp, faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { faGift, faPhone, faMapMarkerAlt, faEnvelope, faTrain, faMusic, faKey, faQuestion, faUsers, faComments, faCalendar, faLock, faUserSecret, faEyeDropper } from '@fortawesome/free-solid-svg-icons'
 import MobileHeader from './mobileHeader'
@@ -13,6 +14,7 @@ import favicon32 from "../../images/eenash/img/favicons/favicon-32x32.png";
 import favicon96 from "../../images/eenash/img/favicons/favicon-96x96.png";
 
 library.add(faFacebookF, faTwitter, faInstagram, faGift, faPhone, faMapMarkerAlt, faEnvelope, faTrain, faMusic, faKey, faQuestion, faUsers, faComments, faCalendar, faLock, faUserSecret, faEyeDropper, faTripadvisor, faYelp, faGoogle);
+config.autoAddCss = false
 
 class EeNashLayout extends React.Component {
   componentDidMount() {
