a5707d8a8fe4729bbb0c650659914efd786a1fe4
Spencer Rowland <jsrowland10@gmail.com>
2020-03-11 12:46:20 -0500

    test escapekit widget script

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,5 +3,6 @@ import React from "react"
 export default () => (
   <div>
     <h1>Just testing</h1>
+    <script type="text/javascript" src="https://staging.escapekit.co/lbwidget.js?lid=2zi3qzgb"></script>
   </div>
 )
