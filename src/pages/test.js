77e625406cf6e4d1932b7f79fc36b462c4aa05d1
Spencer Rowland <jsrowland10@gmail.com>
2020-03-12 10:17:41 -0500

    testing lb widget script

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,6 +3,6 @@ import React from "react"
 export default () => (
   <div>
     <h1>Just testingx</h1>
-    <div dangerouslySetInnerHTML='<script type="text/javascript" src="https://staging.escapekit.co/lbwidget.js?lid=2zi3qzgb"></script>'></div>
+    <div dangerouslySetInnerHTML="<script type='text/javascript' src='https://staging.escapekit.co/lbwidget.js?lid=2zi3qzgb'></script>"></div>
   </div>
 )
