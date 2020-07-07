41461e01bf49070c92ce424be2863f6aab0a1056
Spencer Rowland <jsrowland10@gmail.com>
2020-07-07 10:44:56 -0500

    testing

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,13 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { Helmet } from "react-helmet"
 import BaseLayout from "../components/baseLayout"
 
 const SecondPage = () => (
   <BaseLayout>
+    <Helmet>
+        <title>Testing</title>
+        <script defer type="text/javascript" src="https://www.escapekit.co/lbwidget-v2.js?lid=x0sgb9gg"></script>
+    </Helmet>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <div style={{width: "500px", height: "500px"}}>
+    <div id="ek-lb">
     </div>
     <Link to="/">Go back to the homepage</Link>
   </BaseLayout>
